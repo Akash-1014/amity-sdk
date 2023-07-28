ec633891c8308f94fa73ee3d030a275f269c8735
Akash <kumar.akash.antiersolutions.com>
2023-07-28 11:13:10 +0530

    fsadf

---
+++ src/component/LiveStream.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StreamRepository, StreamResolutions } from '@amityco/js-sdk'
+
+const LiveStream = () => {
+  return (
+    <div>LiveStream</div>
+  )
+}
+
+export default LiveStream
